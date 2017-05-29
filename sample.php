a8cd58b6c5fcff1a6859f2459e47175e721e0d93
r_ohzeki <r_ohzeki@yumemi.co.jp>
2017-05-29 13:39:40 +0900

    first commit

---
+++ sample.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: r_ohzeki
+ * Date: 2017/05/26
+ * Time: 19:04
+ */
+
+namespace sample_test;
+
+class SampleTest
+{
+
+    public function getType()
+    {
+        $test = "test";
+        return 1;
+    }
+
+    public function getName(){
+        return 0;
+    }
+
+    public function getAll()
+    {
+        return 3;
+    }
+
+    public function getAll23()
+    {
+        return 3;
+    }
+
+    public function getAll23()
+    {
+        return 3;
+    }
+
+        public function getAll235()
+    {
+        return 3;
+    }
+
+    public function getAll26()
+    {
+        $test2= "test";
+        return 3;
+    }
+}
